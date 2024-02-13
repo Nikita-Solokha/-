59a2507dcb202f41614a20ac69d1251a15d4e83a
Nikita-Solokha <147943333+Nikita-Solokha@users.noreply.github.com>
2024-02-13 14:20:38 +0300

    Add files via upload

---
+++ 11111.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> arr;
+    int num;
+
+    std::cout << "Введите числа массива: ";
+    while (std::cin >> num) {
+        arr.push_back(num);
+        if (std::cin.peek() == '\n') {
+            break;
+        }
+    }
+
+    std::vector<int> even_numbers;
+    for (int num : arr) {
+        if (num % 2 == 0) {
+            even_numbers.push_back(num);
+        }
+    }
+
+    std::cout << "Четные числа в массиве: ";
+    for (int num : even_numbers) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Количество четных чисел: " << even_numbers.size() << std::endl;
+
+    return 0;
+}
+
+//в теории для лабы очень важные страницы : 6, 18
---
+++ 22222.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main() {
+    std::string input;
+    std::vector<int> array;
+
+    std::cout << "Введите элементы массива: ";
+    std::getline(std::cin, input);
+
+    std::istringstream iss(input);
+    int num;
+    while (iss >> num) {
+        array.push_back(num);
+    }
+
+    int even_sum = 0;
+    for (int num : array) {
+        if (num % 2 == 0) {
+            even_sum += num;
+        }
+    }
+
+    std::cout << "Сумма четных элементов в массиве: " << even_sum << std::endl;
+
+    return 0;
+}
